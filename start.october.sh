268d1129f829033ebef31dca9aacff78ab57f41e
notwin <notwin@users.noreply.github.com>
2016-04-14 19:49:49 +0800

    Update start.october.sh

--- start.october.sh
+++ start.october.sh
@@ -11,5 +11,8 @@ mysql -uroot -proot -e"create database october;"
 php artisan october:up
 php artisan october:util compile assets
 
+sudo rm /var/www/html
+sudo ln -s -b  ~/workspace /var/www/html
+
 sudo php5enmod mcrypt
 sudo service apache2 restart
