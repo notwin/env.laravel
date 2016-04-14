cc5dd5914a16dc68bf00ea9c642b6cf9eab745d8
notwin <notwin@users.noreply.github.com>
2016-04-14 19:27:04 +0800

    Update start.laravel.sh

--- start.laravel.sh
+++ start.laravel.sh
@@ -5,14 +5,9 @@ composer create-project laravel/laravel la dev-master
 mv la/* ./
 chmod -R 0777 ./storage
 mv ./env.laravel/laravel.env ./.env
+mysql -uroot -proot -e"create database october;" 
 php artisan key:generate
 ln -s -b  ~/workspace/public /var/www/html
-
-sed -i "s/www-data/$(whoami)/g" /etc/apache2/envvars
-chown $(whoami):$(whoami) /home/$(whoami)/workspace -R
-a2enmod rewrite
-source /etc/apache2/envvars
-tail -F /var/log/apache2/* &
-service apache2 start
+service apache2 restart
 
 
