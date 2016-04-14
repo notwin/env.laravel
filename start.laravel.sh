54fbaceada232e1710141daa61074400631a0ff6
notwin <notwin@users.noreply.github.com>
2016-04-14 19:39:20 +0800

    Update start.laravel.sh

--- start.laravel.sh
+++ start.laravel.sh
@@ -5,7 +5,7 @@ composer create-project laravel/laravel la dev-master
 mv la/* ./
 chmod -R 0777 ./storage
 mv ./env.laravel/laravel.env ./.env
-mysql -uroot -proot -e"create database october;" 
+mysql -uroot -proot -e"create database laravel_db;" 
 php artisan key:generate
 ln -s -b  ~/workspace/public /var/www/html
 
