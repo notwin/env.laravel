ebbd0573011009ef7cf03ce5ef9ac8741927fc13
notwin <notwin@users.noreply.github.com>
2016-04-14 18:22:04 +0800

    Create start.laravel.sh

---
+++ start.laravel.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ~/workspace
+composer create-project laravel/laravel la dev-master
+mv la/* ./
+chmod -R 0777 ./storage
+mv ./env.laravel/laravel.env ./.env
+php artisan key:generate
+ln -s -b  ~/workspace/public /var/www/html
+service apache2 restart
+
