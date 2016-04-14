06f39cac5e0da039071571b6ef33bcb341084df6
notwin <notwin@users.noreply.github.com>
2016-04-14 19:31:56 +0800

    Update start.october.sh

--- start.october.sh
+++ start.october.sh
@@ -7,7 +7,6 @@ chmod -R 0777 ./storage
 mv ./env.laravel/october.htaccess ./.htaccess
 mv ./env.laravel/laravel.env ./.env
 mv ./env.laravel/october.mysql.config ./config/dev/database.php
-php artisan key:generate
 mysql -uroot -proot -e"create database october;" 
 php artisan october:up
 php artisan october:util compile assets
