d1bb53bf8c355cffe999346566e9ae4b2bf8aafa
notwin <notwin@users.noreply.github.com>
2016-04-14 19:09:06 +0800

    Update start.october.sh

--- start.october.sh
+++ start.october.sh
@@ -10,10 +10,3 @@ php artisan key:generate
 php artisan october:up
 php artisan october:util compile assets
 
-
-sed -i "s/www-data/$(whoami)/g" /etc/apache2/envvars
-chown $(whoami):$(whoami) /home/$(whoami)/workspace -R
-a2enmod rewrite
-source /etc/apache2/envvars
-tail -F /var/log/apache2/* &
-service apache2 start
