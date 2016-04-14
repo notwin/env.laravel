840a8e0a7d17d75dd9ce926f8f1ee177f9328b20
notwin <notwin@users.noreply.github.com>
2016-04-14 18:38:46 +0800

    Update start.laravel.sh

--- start.laravel.sh
+++ start.laravel.sh
@@ -7,5 +7,12 @@ chmod -R 0777 ./storage
 mv ./env.laravel/laravel.env ./.env
 php artisan key:generate
 ln -s -b  ~/workspace/public /var/www/html
-service apache2 restart
+
+sed -i "s/www-data/$(whoami)/g" /etc/apache2/envvars
+chown $(whoami):$(whoami) /home/$(whoami)/workspace -R
+a2enmod rewrite
+source /etc/apache2/envvars
+tail -F /var/log/apache2/* &
+service apache2 start
+
 
